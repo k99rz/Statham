02beacfd193b3661e02fae2bdcf1119c9fe6643f
k99rz <none>
2016-05-09 15:36:24 +0200

    Added support for Carthage dependency manager. It requires an Xcode project that builds a dynamic framework.

---
+++ Statham/Statham.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Statham.
+FOUNDATION_EXPORT double StathamVersionNumber;
+
+//! Project version string for Statham.
+FOUNDATION_EXPORT const unsigned char StathamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Statham/PublicHeader.h>
+
+
